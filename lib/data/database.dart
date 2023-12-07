5f8a07c66048b899479f3673b7464da07d1cb13f
Leticia <leticiasousassilva@hotmail.com>
2023-12-06 21:31:42 -0300

    banco de dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,9 +2,12 @@
 import 'package:nossa_lista_de_desenhos/data/cartoon_dao.dart';
 import 'package:nossa_lista_de_desenhos/data/user_dao.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart';
 
 Future<Database> getDatabase() async {
+  sqfliteFfiInit(); // Certifique-se de chamar isso antes de openDatabase
+  databaseFactory = databaseFactoryFfi; // Configura a fábrica do banco de dados
   final String path = join(await getDatabasesPath(), 'cartoon.db');
   return openDatabase(path, onCreate: (db, version) {
     db.execute(CartoonDao.tableSql);
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:nossa_lista_de_desenhos/components/cartoon.dart';
 import 'package:nossa_lista_de_desenhos/data/cartoon_dao.dart';
-import 'package:nossa_lista_de_desenhos/data/cartoon_inherited.dart';
-import 'package:sqflite_common/sqlite_api.dart';
+//import 'package:nossa_lista_de_desenhos/data/cartoon_inherited.dart';
+//import 'package:sqflite_common/sqlite_api.dart';
 
 
 //import '../components/cartoon.dart';
 import 'form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
-  const InitialScreen({super.key});
+  const InitialScreen({Key? key}) : super(key: key);
 
   @override
   State<InitialScreen> createState() => _InitialScreenState();
@@ -18,81 +18,44 @@ class InitialScreen extends StatefulWidget {
 class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Our Cartoon List'),
         backgroundColor: Colors.red,
         leading: Container(),
         actions: [
-          IconButton(onPressed: (){setState(() {});}, icon: Icon(Icons.refresh))
+          IconButton(onPressed: () => _reloadScreen(), icon: const Icon(Icons.refresh)),
         ],
       ),
       body: Padding(
-        padding: EdgeInsets.only(top: 8, bottom: 70),
+        padding: const EdgeInsets.only(top: 8, bottom: 70),
         child: FutureBuilder<List<Cartoon>>(
-            future: CartoonDao().findAll(),
-            builder: (context, snapshot) {
-              List<Cartoon>? items = snapshot.data;
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return Center(
-                    child: Column(
-                      children: [
-                        CircularProgressIndicator(),
-                        Text('Carregando'),
-                      ],
-                    ),
+          future: CartoonDao().findAll(),
+          builder: (context, snapshot) {
+            try {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData && snapshot.data != null) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Cartoon desenho = snapshot.data![index];
+                      return desenho;
+                    },
                   );
-                  break;
-                case ConnectionState.waiting:
-                  return Center(
-                    child: Column(
-                      children: [
-                        CircularProgressIndicator(),
-                        Text('Carregando'),
-                      ],
-                    ),
-                  );
-                  break;
-                case ConnectionState.active:
-                  return Center(
-                    child: Column(
-                      children: [
-                        CircularProgressIndicator(),
-                        Text('Carregando'),
-                      ],
-                    ),
-                  );
-                  break;
-                case ConnectionState.done:
-                  if (snapshot.hasData && items != null) {
-                    if (items.isNotEmpty) {
-                      return ListView.builder(
-                          itemCount: items.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            final Cartoon desenho = items[index];
-                            return desenho;
-                          });
-                    }
-                    return Center(
-                      child: Column(
-                        children: [
-                          Icon(Icons.error_outline, size: 128),
-                          Text(
-                            'Não há nenhum Desenho',
-                            style: TextStyle(fontSize: 32),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                  return Text(' Erro ao carregar Desenhos ');
-                  break;
+                } else {
+                  return _buildNoCartoonWidget();
+                }
+              } else if (snapshot.connectionState == ConnectionState.waiting ||
+                  snapshot.connectionState == ConnectionState.active) {
+                return _buildLoadingWidget();
+              } else {
+                return _buildErrorWidget('Erro ao carregar Desenhos: ${snapshot.error}');
               }
-              return Text('Erro desconhecido');
-            }),
+            } catch (error) {
+              return _buildErrorWidget('Erro desconhecido: $error');
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -103,12 +66,55 @@ class _InitialScreenState extends State<InitialScreen> {
                 cartoonContext: context,
               ),
             ),
-          ).then((value) => setState(() {
-                print('Recarregando a tela inicial');
-              }));
+          ).then((value) => _reloadScreen());
         },
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Widget _buildNoCartoonWidget() {
+    return Center(
+      child: Column(
+        children: const [
+          Icon(Icons.error_outline, size: 128),
+          Text(
+            'Não há nenhum Desenho',
+            style: TextStyle(fontSize: 32),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoadingWidget() {
+    return Center(
+      child: Column(
+        children: const [
+          CircularProgressIndicator(),
+          Text('Carregando'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildErrorWidget(String errorMessage) {
+    return Center(
+      child: Column(
+        children: [
+          Icon(Icons.error_outline, size: 128),
+          Text(
+            errorMessage,
+            style: TextStyle(fontSize: 32),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _reloadScreen() {
+    setState(() {
+      print('Recarregando a tela inicial');
+    });
   }
 }
